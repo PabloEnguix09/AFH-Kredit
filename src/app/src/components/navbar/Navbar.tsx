6c16af79d788b4a9be5b983f33c33df9c106f898
PabloEnguix09 <pabloenguixllopis@gmail.com>
2023-10-09 20:45:30 +0200

    se acaba la bateria xd

--- src/app/src/components/navbar/Navbar.tsx
+++ src/app/src/components/navbar/Navbar.tsx
@@ -1,28 +1,40 @@
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css"
 import { Link } from "react-router-dom";
+import burger from "../../img/burger_menu.svg"
 
 function Navbar() {
+    const [ventana, setVentana] = useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    })
+
+    useEffect(() => {
+        setVentana({
+            height: window.innerHeight,
+            width: window.innerWidth
+        })
+    }, [])
+    
     return(
         <nav className={styles.navbar}>
             <a href="/"><img src={require("../../img/Logo.png")} alt="Logo AFH Kredit" /></a>
-
             <ul className={styles.enlaces}>
                 <li><a href="/nosotros">Nosotros</a></li>
                 <li><a href="/simulador">Simula tu hipoteca</a></li>
                 <li><a href="/contacto">Contacto</a></li>
                 <li><a href="/blog">Blog</a></li>
             </ul>
-
             <div>
-                <select name="" id="">
+                {/*<select name="" id="">
                     <option value="ES">ES</option>
                     <option value="VA">VA</option>
                     <option value="EN">EN</option>
-                </select>
-
+                </select> */}
                 <Link to={"/login"}>
                     <button className={styles.loginBtn}>Iniciar sesión</button>
                 </Link>
+                <img className={styles.burger} src={burger} alt="Menú móvil" />
             </div>
         </nav>
     )
